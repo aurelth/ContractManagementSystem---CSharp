20dc50296440f6a4d37669a19d04f34776b3ccbb
aurelth <aurelth@icloud.com>
2020-05-30 01:20:50 -0300

    Add project files.

---
+++ ContractManagementSystem/Entities/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContractManagementSystem.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+        
+    }   
+}
---
+++ ContractManagementSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ContractManagementSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
